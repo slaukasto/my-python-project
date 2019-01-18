454d952acaaa0cae6c6c85fb2e71b8d7df0ea6dd
Seppo Laukasto <Seppo.Laukasto@enfo.fi>
2019-01-18 11:02:15 +0200

    3th Commit

--- githup.py
+++ githup.py
@@ -2,7 +2,10 @@ print("This is my first GitHub Project")
 # -- asenna git client
 # --- https://gitforwindows.org/
 # -- Git-repon luotni omalle koneelle: git init
-# -- käynnistä GitHub
+# -- käynnistä GitHub ($-kehote)
 #-- mene koneesi Git-hakemsitoon (tee se)
-# -- git add .
+# -- 1) Editoi, talleta
+# -- 2) $ git add . (lisää lokaalit muutokset)
+# -- 3) $ git commit -m "My comment"
+# -- 4) $ git push -u origin maste
 # -- lisäviiva
