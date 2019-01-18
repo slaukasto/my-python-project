70244d8e7a82c8f2ac7450cf34281de11b5028bb
Seppo Laukasto <Seppo.Laukasto@enfo.fi>
2019-01-18 10:57:25 +0200

    2nd Commit

--- githup.py
+++ githup.py
@@ -2,3 +2,7 @@ print("This is my first GitHub Project")
 # -- asenna git client
 # --- https://gitforwindows.org/
 # -- Git-repon luotni omalle koneelle: git init
+# -- käynnistä GitHub
+#-- mene koneesi Git-hakemsitoon (tee se)
+# -- git add .
+# -- lisäviiva
